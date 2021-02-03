1f94c8d5705a0b4aaf5840a5931d49d3d5cac9a7
matheusparro <mathparro@gmail.com>
2021-02-03 13:27:15 -0300

    Porta Alterada

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const routes = require('./routes')
 require ('./DataBase')
+let PORT = process.env.PORT || '3333';
 const app = express()
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333)
+app.listen(PORT)
