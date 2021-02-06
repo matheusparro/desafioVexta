076b8c9ed54d8b1100efa33652995064bf8e3b69
matheusparro <mathparro@gmail.com>
2021-02-06 20:22:45 -0300

    Confg Heroku

--- config/config.js
+++ config/config.js
@@ -82,14 +82,14 @@
 // }
 
 module.exports = {
-  // host: 'ec2-52-205-3-3.compute-1.amazonaws.com',
-  // username:'hfailttvrveoxr',
-  // password:'67c4fb55cb1cbd2707542ab965c6a272cee3d5c3dd2771a62495c47d547be845',
-  // database:'dbcen3s14hknqs',
-  url:process.env.DATABASE_URL,
+  host: 'ec2-52-205-3-3.compute-1.amazonaws.com',
+  username:'hfailttvrveoxr',
+  password:'67c4fb55cb1cbd2707542ab965c6a272cee3d5c3dd2771a62495c47d547be845',
+  database:'dbcen3s14hknqs',
+  // url:process.env.DATABASE_URL,
   dialect: 'postgres',
   port : '5432',
-  ssl: true,
+  ssl: false,
   define:  {
       timestamp: true,
       underscored:true,
