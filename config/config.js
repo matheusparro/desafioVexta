8001638f0f74c2a8f54da9b380e9966e608344ef
matheusparro <mathparro@gmail.com>
2021-02-06 20:03:51 -0300

    Confg Heroku

--- config/config.js
+++ config/config.js
@@ -71,13 +71,28 @@
 //   },
 // }
 
+// module.exports = {
+//   dialect: 'postgres',
+//   url: 'process.env.DATABASE_URL',
+//   define:  {
+//       timestamp: true,
+//       underscored:true,
+//       freezeTableName: false,
+//   },
+// }
+
 module.exports = {
+  host: 'ec2-52-205-3-3.compute-1.amazonaws.com',
+  username:'hfailttvrveoxr',
+  password:'67c4fb55cb1cbd2707542ab965c6a272cee3d5c3dd2771a62495c47d547be845',
+  database:'dbcen3s14hknqs',
   dialect: 'postgres',
-  url: 'process.env.DATABASE_URL',
+  port : '5432',
   define:  {
       timestamp: true,
       underscored:true,
       freezeTableName: false,
   },
 }
+
 
